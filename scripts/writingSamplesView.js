24d1e4eee4661ea1b57099343835a172c9e1c4eb
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-09-21 23:32:42 -0700

    Added writingSamplesView.js for samples display functionality, category filter

---
+++ scripts/writingSamplesView.js
@@ -0,0 +1,45 @@
+var articleView = {};
+
+articleView.populateFilters = function() {
+  $('article').not('template').each(function() {
+    var category, optionTag;
+    category = $(this).attr('data-category');
+    optionTag = '<option value"' + category + '">' + category + '</option>';
+
+    if ($('#category-filter option[value="' + category + '"]').length === 0) {
+      $('#category-filter').append(optionTag);
+    }
+  });
+};
+
+articleView.handleCategoryFilter = function() {
+  $('#category-filter').on('change', function() {
+    var selectedValue = $(this).val();
+    if (selectedValue) {
+      var $samples = $('#samples article');
+      $samples.each(function() {
+        $(this).hide();
+        if (selectedValue === $(this).attr('data-category')) {
+          $(this).fadeIn('slow');
+        }
+      });
+    } else {
+      var $samples = $('#samples article');
+      $samples.not('template').each(function() {
+        $(this).show();
+      });
+    }
+    $('#category-filter').val('');
+  });
+};
+
+articleView.handleMainNav = function(event) {
+  $('main-nav').on('click', '.tab', function() {
+    $('.tab-content').hide();
+    event.preventDefault();
+
+    var selectedContent = $(this).data('content');
+    $('#' + $selectedContent).fadeIn('slow');
+  });
+  $('main-nav .tab:first').click();
+};
